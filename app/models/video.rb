7c566aa8086e60bc9d670e1566e3d3b09ade33a0
Leandro <email@dominio.com.br>
2017-04-10 18:29:24 -0300

    videos controller

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
 
   validates :url, :title, :description, :course_id, presence: true
+  validates_numericality_of :course_id, :category_id
 
   belongs_to :course
   belongs_to :category

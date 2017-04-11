41d459ca413a72bdc9b7c3a48f0ebfde1f007aae
Leandro <email@dominio.com.br>
2017-04-11 12:21:47 -0300

    archive form

--- app/uploaders/archive_uploader.rb
+++ app/uploaders/archive_uploader.rb
@@ -14,6 +14,13 @@ class ArchiveUploader < CarrierWave::Uploader::Base
     "#{model.class.to_s.underscore}/#{model.id}"
   end
 
+  def default_url
+    # For Rails 3.1+ material pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+    "no_cover_image.gif"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:

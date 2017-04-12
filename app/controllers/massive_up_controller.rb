d4532b50ca3346bb4e9e7dae0cc3514af0cafe84
Leandro <email@dominio.com.br>
2017-04-12 10:27:25 -0300

    massive controller

--- app/controllers/massive_up_controller.rb
+++ app/controllers/massive_up_controller.rb
@@ -8,60 +8,22 @@ class MassiveUpController < ApplicationController
     puts '============================ CREATE ===================================='
     params[:archive][:url].each do |url|
       extension = get_extension(url.original_filename)
+      get_set_archive(url)
+
       case extension
         when '.skp'
           puts '============================== WHEN SKP! INICIO++++++++++++++++++++++++++++++++++++'
-          puts "============= PARAMS CREATE ====== url?  #{url}   ============================  "
-          @archive = Archive.find_by_name(url.original_filename[0..-5])
-          if @archive
-            @archive.url = url
-            @archive.save
-          else
-          @archive = Archive.new()
-          @archive.category_id = params[:archive][:category_id]
-          @archive.course_id = params[:archive][:course_id]
-          @archive.description = params[:archive][:description]
-          @archive.name = url.original_filename[0..-5]
+
           @archive.url = url
-          puts '============================== WHEN SKP! FINAL++++++++++++++++++++++++++++++++++++'
           @archive.save
-            # redirect_to archives_path, :notice => "deu crepe #{@archive.errors.full_messages}"
-          end
         when '.png'
           puts '============================== WHEN PNG!+ INICIO+++++++++++++++++++++++++++++++++++'
-          @archive = Archive.find_by_name(url.original_filename[0..-5])
-          if @archive
-            @image = @archive.images.new(image_params)
-            @image.url = url
-            @image.title = url.original_filename[0..-5]
-            @image.description = params[:archive][:description]
+          @image = @archive.images.new(image_params)
+          @image.url = url
+          @image.title = url.original_filename[0..-5]
+          @image.description = params[:archive][:description]
 
-            @image.save
-          else
-            puts '============================== WHEN PNG!+ ELSE +++++++++++++++++++++++++++++++++++'
-            @archive = Archive.new(archives_params)
-            @archive.category_id = params[:archive][:category_id]
-            @archive.course_id = params[:archive][:course_id]
-            @archive.description = params[:archive][:description]
-            @archive.name = url.original_filename[0..-5]
-            puts "============= PARAMS CREATE ====== archive valid?  #{@archive.valid?}   ============================  "
-
-            @archive.save
-
-            @image = Image.new
-            @image.imageable_id = @archive.id
-            @image.imageable_type = Archive
-            @image.url = url
-            @image.title = url.original_filename[0..-5]
-            @image.description = params[:archive][:description]
-            puts "============= PARAMS CREATE ====== url  #{@image.url}   ============================  "
-            puts "============= PARAMS CREATE ====== title  #{@image.title}   ============================  "
-            puts "============= PARAMS CREATE ====== description  #{@image.description}   ============================  "
-            puts "============= PARAMS CREATE ====== valid?  #{@image.valid?}   ============================  "
-            puts "============= PARAMS CREATE ====== errors?  #{@image.errors.full_messages}   ============================  "
-            @image.save
-            puts '============================== WHEN PNG!+ FINAL+++++++++++++++++++++++++++++++++++'
-          end
+          @image.save
       end
     end
     redirect_to archives_path, notice: t('views.image.create')
@@ -82,6 +44,19 @@ class MassiveUpController < ApplicationController
     size = filename.length
     final_size = size - 4
     return  filename[final_size..size]
+
+  end
+
+  def get_set_archive(url)
+    @archive = Archive.find_by_name(url.original_filename[0..-5])
+    if not @archive
+      @archive = Archive.new()
+      @archive.category_id = params[:archive][:category_id]
+      @archive.course_id = params[:archive][:course_id]
+      @archive.description = params[:archive][:description]
+      @archive.name = url.original_filename[0..-5]
+      @archive.save
+    end
 
   end
 

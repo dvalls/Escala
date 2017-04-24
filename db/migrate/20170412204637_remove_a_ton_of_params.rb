9a9e3cf70578dcc1a566919eb0f91e2b05371372
Leandro <email@dominio.com.br>
2017-04-24 12:03:15 -0300

    ajustado migração

--- db/migrate/20170412204637_remove_a_ton_of_params.rb
+++ db/migrate/20170412204637_remove_a_ton_of_params.rb
@@ -1,10 +1,17 @@
 class RemoveATonOfParams < ActiveRecord::Migration[5.0]
   def up
-    remove_column :content_videos, :category_id
-    remove_column :content_videos, :course_id
+    # rename_table :videos, :content_videos
+    create_table :content_videos do |t|
+      t.string :title
+      t.string :description
+      t.string :url
+    end
+
+    # remove_column :content_videos, :category_id
+    # remove_column :content_videos, :course_id
     remove_column :archives, :course_id
 
-    add_column :content_videos, :page_id, :integer
+    # add_column :content_videos, :page_id, :integer
     add_column :archives, :page_id, :integer
 
     create_table :pages do |t|

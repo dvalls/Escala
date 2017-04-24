7ba97a193972e82cca8a22dbae0148ad91500dd9
Leandro <email@dominio.com.br>
2017-04-24 12:12:01 -0300

    ajustado migrações

--- db/migrate/20170413141605_create_templates.rb
+++ db/migrate/20170413141605_create_templates.rb
@@ -1,6 +1,6 @@
 class CreateTemplates < ActiveRecord::Migration[5.0]
   def up
-    create_table :content_blocks do |t|
+    create_table :templates do |t|
       t.integer :page_id, index: true
     end
 
@@ -13,6 +13,6 @@ class CreateTemplates < ActiveRecord::Migration[5.0]
     # add_column :pages, :template_id, :integer
     add_column :pages, :template_type, :string
 
-    drop_table :templates
+    drop_table :content_blocks
   end
 end

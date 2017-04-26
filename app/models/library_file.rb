c547c8f5ed470282a873ec68d245e24b12d2adc9
Leandro <email@dominio.com.br>
2017-04-26 18:00:06 -0300

    download de arquivos da biblioteca

--- app/models/library_file.rb
+++ app/models/library_file.rb
@@ -15,18 +15,18 @@ class LibraryFile < ActiveRecord::Base
     return category.name
   end
 
-  def download_url
-  S3 = AWS::S3.new.buckets[ 'bucket_name' ] # This can be done elsewhere as well,
-                                            # e.g config/environments/development.rb
-
-  url_options = {
-    expires_in:                   60.minutes,
-    use_ssl:                      true,
-    # response_content_disposition: "attachment; filename=\"#{attachment_file_name}\""
-    # "uploads/#{model.class.to_s.underscore}/#{model.id}"
-  }
-
-  S3.objects[ self.path ].url_for( :read, url_options ).to_s
-end
+#   def download_url
+#   S3 = AWS::S3.new.buckets[ 'bucket_name' ] # This can be done elsewhere as well,
+#                                             # e.g config/environments/development.rb
+#
+#   url_options = {
+#     expires_in:                   60.minutes,
+#     use_ssl:                      true,
+#     # response_content_disposition: "attachment; filename=\"#{attachment_file_name}\""
+#     # "uploads/#{model.class.to_s.underscore}/#{model.id}"
+#   }
+#
+#   S3.objects[ self.path ].url_for( :read, url_options ).to_s
+# end
 
 end

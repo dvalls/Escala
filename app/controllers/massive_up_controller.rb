a8c440194d5424da5dc690a94121f0fcdab8ca8b
Leandro <email@dominio.com.br>
2017-04-27 15:05:32 -0300

    ordenar arquivos criados

--- app/controllers/massive_up_controller.rb
+++ app/controllers/massive_up_controller.rb
@@ -46,7 +46,9 @@ class MassiveUpController < ApplicationController
 
   def get_set_archive(url)
     name = (url.original_filename[0..-5]).downcase
+    #Procura um arquivo de biblioteca existente a partir do nome do arquivo
     @library_file = LibraryFile.find_by_name(name)
+    # Caso não encontre um arquivo de biblioteca existente
     if not @library_file
       @library_file = LibraryFile.new()
       @library_file.category_id = params[:library_file][:category_id]
@@ -57,10 +59,11 @@ class MassiveUpController < ApplicationController
   end
 
   def get_set_image
+    # caso o arquivo de biblioteca não tenha uma image associada
     if @library_file.image == nil
       @image = Image.new(imageable_id: @library_file.id, imageable_type: 'LibraryFile')
       # @image = @library_file.image.new(image_params)
-    else
+    else # Se tiver, a imagem sera substituida
       @image = @library_file.image
     end
   end

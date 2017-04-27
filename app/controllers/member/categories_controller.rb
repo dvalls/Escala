bbb54607d42e15bf9261617a6a0dc3ab0e5f22b4
Leandro <email@dominio.com.br>
2017-04-27 09:56:09 -0300

    ordenando arquivos de biblioteca

--- app/controllers/member/categories_controller.rb
+++ app/controllers/member/categories_controller.rb
@@ -10,8 +10,8 @@ class Member::CategoriesController <  Member::MemberAreaController # Application
   end
 
   def show
-    @category = Category.find(params[:id])
-    @subcategories = @category.subcategories
+    category = Category.find(params[:id])
+    @library_files = category.library_files.order(:name)
     @content_block = ContentBlock.find(params[:content_block_id])
     @content_library_group = ContentLibraryGroup.find(params[:content_library_group_id])
     @course = Course.find(@content_library_group.course_id)

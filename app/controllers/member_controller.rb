c97e46cd8088707be1384aa7fe546dfcc5ad8bf1
Leandro <email@dominio.com.br>
2017-05-04 12:35:24 -0300

    ajustes do controller

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,27 +1,9 @@
 class MemberController < ApplicationController
-  before_action :set_courses, only: [:index, :show]
-
   before_filter :student_logged?
-  # layout 'member'#, :only => [:index]
   layout 'member'
-
-  def home
-    @carousels = Carousel.all.order(order: :asc)
-  end
-
 
   def index
     @student = get_student
-  end
-
-  def show
-    @course = Course.friendly.find(params[:id])
-    @pages = @course.pages
-  end
-
-  private
-
-  def set_courses
     case session[:admin]
       when true
         @courses = Course.all

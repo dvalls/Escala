03a9ccba9c0f72e80338d855637bac5afd7bf894
Leandro <email@dominio.com.br>
2017-05-04 12:31:45 -0300

    ajustes gerais

--- app/controllers/member/member_area_controller.rb
+++ app/controllers/member/member_area_controller.rb
@@ -3,24 +3,21 @@ class Member::MemberAreaController < ApplicationController
   before_action :student_logged?
 
   helper_method :get_page_contents
+
   def get_page_contents
     @docs_groups = []
     @page_categories = []
     @content_videos = []
     @page.content_blocks.each do |block|
-
       case block.contentable_type
-
         when 'ContentVideoGroup'
           block.contentable.content_videos.each do |video|
             if not video.blank?
               @content_videos << video
             end
           end
-
         when 'ContentLibraryGroup'
           @page_categories = block.contentable.categories
-
         when 'ContentDocsGroup'
           @docs_groups = block.contentable.docs
       end

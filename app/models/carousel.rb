a8423fccd015413d82bc59c34e85a126a7e4b588
Leandro <email@dominio.com.br>
2017-05-05 14:42:59 -0300

    ajuste carousel

--- app/models/carousel.rb
+++ app/models/carousel.rb
@@ -3,7 +3,7 @@ class Carousel < ActiveRecord::Base
   translates :title, :description
   mount_uploader :image_url, CarouselUploader
 
-  validates_presence_of :image_url, :order
+  validates_presence_of :image_url, :order, :title, :description
 
   belongs_to :course
 

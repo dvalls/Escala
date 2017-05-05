1a4d29647de144e0bb21e0c018d1810264cb0f28
Leandro <email@dominio.com.br>
2017-05-05 16:05:49 -0300

    tabelas com bordas

--- app/controllers/member/content_videos_controller.rb
+++ app/controllers/member/content_videos_controller.rb
@@ -6,6 +6,5 @@ class Member::ContentVideosController < Member::MemberAreaController # Applicati
     @course = Course.find(@block.contentable.course_id)
     @pages = @course.pages
     @content_video = ContentVideo.find(params[:id])
-    # @video_groups = block.contentable #@content_video.content_video_groups.where(:course_id => @course.id)
   end
 end
